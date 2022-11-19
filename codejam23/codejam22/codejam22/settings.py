6f1c5d1167eb34577fdb2609789a31ce61cd8fc9
Tim Pham <timmyhoa34@gmail.com>
2022-11-19 12:34:07 -0500

    Added elementary chat function

--- codejam23/codejam22/codejam22/settings.py
+++ codejam23/codejam22/codejam22/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "channels",
     "chat",
     "main",
     "api",
@@ -42,6 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
 ]
+
+ASGI_APPLICATION = "codejam22.asgi.application"
+
+CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
